6c95f0e38c5a1e941996239988ec954fe53afca2
skwidhubz <tommaxwellparker@gmail.com>
2023-02-08 13:59:39 +1100

    style sheet

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const inquirer = require('./inquirer');
+const Employee = require('./lib/Employee')
 
 
 function init(){
 
+    const QUESTIONS = [
+        {
+            type: 'input',
+            message: 'what is your name?',
+            name: 'name'
+
+        }
+    ]
+    }
+
 inquirer
 .prompt( QUESTIONS )
 
+
 .then((response) => {
+
+    var people = []
+    var person = new Employee(response.name)
+    people.push(person)
+    return people
 
 });
 
 
-}
 
+
+
+// build HTML first as a template
+
+`the name is ${people[0].person.name}`
